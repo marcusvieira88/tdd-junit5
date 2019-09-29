674733df9d2e1ef3e9b02e8389cb0516f91d9ab5
Marcus Vieira <marcus.vieira@elinvar.de>
2019-09-29 22:01:01 +0200

    Add TDD example classes

--- src/test/java/tech/marcusvieira/BankAccountTest.java
+++ src/test/java/tech/marcusvieira/BankAccountTest.java
@@ -3,7 +3,7 @@ package tech.marcusvieira;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -12,13 +12,14 @@ public class BankAccountTest {
 
     private static final Double INITIAL_BANK_ACCOUNT_BALANCE = 0D;
 
-    private BankAccountService bankAccountService;
+    private static BankAccountService bankAccountService;
 
-    @BeforeEach
-    public void init() {
+    @BeforeAll
+    public static void init() {
         bankAccountService = new BankAccountService();
     }
 
+    //Bank account create tests
     @ParameterizedTest
     @ValueSource(ints = {1, 200, 3000})
     public void shouldCreateBankAccountWhenProvidedId(Integer bankAccountId) {
@@ -29,14 +30,15 @@ public class BankAccountTest {
 
     @Test
     public void shouldThrowsExceptionWhenProvidedNullId() {
-        assertThrows(NumberFormatException.class, () -> bankAccountService.create(null));
+        assertThrows(IllegalArgumentException.class, () -> bankAccountService.create(null));
     }
 
     @Test
     public void shouldThrowsExceptionWhenProvidedNegativeId() {
-        assertThrows(NumberFormatException.class, () -> bankAccountService.create(-1));
+        assertThrows(IllegalArgumentException.class, () -> bankAccountService.create(-1));
     }
 
+    //Bank account deposit tests
     @ParameterizedTest
     @ValueSource(doubles = {100, 500, 1000})
     public void shouldIncreaseBalanceWhenDepositMade(Double amount) {
@@ -48,13 +50,13 @@ public class BankAccountTest {
     @Test
     public void shouldThrowsExceptionWhenDepositAmountIsNull() {
         BankAccount bankAccount = new BankAccount(1);
-        assertThrows(NumberFormatException.class, () -> bankAccountService.deposit(bankAccount, null));
+        assertThrows(IllegalArgumentException.class, () -> bankAccountService.deposit(bankAccount, null));
     }
 
     @Test
     public void shouldThrowsExceptionWhenDepositAmountIsNegative() {
         BankAccount bankAccount = new BankAccount(1);
-        assertThrows(NumberFormatException.class, () -> bankAccountService.deposit(bankAccount, -1D));
+        assertThrows(IllegalArgumentException.class, () -> bankAccountService.deposit(bankAccount, -1D));
     }
 
     @Test
@@ -62,6 +64,7 @@ public class BankAccountTest {
         assertThrows(IllegalArgumentException.class, () -> bankAccountService.deposit(null, 100D));
     }
 
+    //Bank account withdrawal tests 
     @ParameterizedTest
     @ValueSource(doubles = {150, 200, 350})
     public void shouldDecreaseBalanceWhenWithdrawalMade(Double amount) {
@@ -73,13 +76,13 @@ public class BankAccountTest {
     @Test
     public void shouldThrowsExceptionWhenWithdrawalAmountIsNull() {
         BankAccount bankAccount = new BankAccount(1);
-        assertThrows(NumberFormatException.class, () -> bankAccountService.withdrawal(bankAccount, null));
+        assertThrows(IllegalArgumentException.class, () -> bankAccountService.withdrawal(bankAccount, null));
     }
 
     @Test
     public void shouldThrowsExceptionWhenWithdrawalAmountIsNegative() {
         BankAccount bankAccount = new BankAccount(1);
-        assertThrows(NumberFormatException.class, () -> bankAccountService.withdrawal(bankAccount, -1D));
+        assertThrows(IllegalArgumentException.class, () -> bankAccountService.withdrawal(bankAccount, -1D));
     }
 
     @Test
